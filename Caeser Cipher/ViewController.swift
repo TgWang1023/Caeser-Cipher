824a90e8b802072cbf112526f37ebf5c06570ff4
Tiange Wang <wtgkevin@gmail.com>
2018-09-19 15:01:10 -0700

    first commit

--- Caeser Cipher/ViewController.swift
+++ Caeser Cipher/ViewController.swift
@@ -8,8 +8,47 @@
 
 import Cocoa
 
+extension String {
+    var asciiValue: Int {
+        return Int(unicodeScalars[unicodeScalars.startIndex].value)
+    }
+}
 class ViewController: NSViewController {
 
+    @IBOutlet weak var textField: NSTextField!
+    @IBOutlet weak var shiftAmount: NSTextField!
+    
+    @IBAction func encryptPressed(_ sender: NSButton) {
+        var str = textField.stringValue
+        var result: String = ""
+        var ascii: [Int] = []
+        for _ in str {
+            ascii.append(str.asciiValue)
+            str.removeFirst()
+        }
+        print(ascii)
+        for i in 0..<ascii.count {
+            ascii[i] += Int(shiftAmount.stringValue)!
+            result += String(UnicodeScalar(ascii[i])!)
+        }
+        textField.stringValue = result
+    }
+    @IBAction func decryptPressed(_ sender: NSButton) {
+        var str = textField.stringValue
+        var result: String = ""
+        var ascii: [Int] = []
+        for _ in str {
+            ascii.append(str.asciiValue)
+            str.removeFirst()
+        }
+        print(ascii)
+        for i in 0..<ascii.count {
+            ascii[i] -= Int(shiftAmount.stringValue)!
+            result += String(UnicodeScalar(ascii[i])!)
+        }
+        textField.stringValue = result
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
